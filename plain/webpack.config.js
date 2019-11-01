7d9da1e74270e82919d04b307edc1d66277664c0
Christoffer G. Thomsen <chris@cgt.name>
2019-11-01 20:56:34 +0100

    Set up webpack-dev-server

--- plain/webpack.config.js
+++ plain/webpack.config.js
@@ -4,6 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist'
+    },
     entry: './src/index.ts',
     module: {
       rules: [
